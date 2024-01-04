ec02b6bf1453820cf860bab29be069db4d9b6891
hunterxcobby <solomonsefah13@gmail.com>
2024-01-04 19:28:24 +0000

    fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,18 +11,18 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
-    binary_tree_t *newnode;
+	binary_tree_t *newnode;
 
-    newnode = malloc(sizeof(binary_tree_t));
-    if (newnode == NULL)
-    {
-        return (NULL); /* Memory failed*/
-    }
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+	{
+		return (NULL); /* Memory failed*/
+	}
 
-    newnode->parent = parent;
+	newnode->parent = parent;
 	newnode->n = value;
 	newnode->left = NULL;
 	newnode->right = NULL;
 
-    return (newnode);
+	return (newnode);
 }
